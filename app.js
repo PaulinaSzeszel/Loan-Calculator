bae91729c5c08a8c248a158140cabb6e79cdb380
Paulina Szeszel Web Developer <86908540+PaulinaSzeszel@users.noreply.github.com>
2022-10-28 19:54:55 +0200

    added the loader

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 //Listener for submit
-document
-  .getElementById('loan-form')
-  .addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  //Hider results
+  document.getElementById('results').style.display = 'none';
+  //Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 function calculateResults(e) {
   //UI vars
@@ -24,15 +31,25 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    //Show results
+    document.getElementById('results').style.display = 'block';
+
+    //Hide  loader
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }
 
 //Show error
 function showError(error) {
+  //Show results
+  document.getElementById('results').style.display = 'none';
+
+  //Hide  loader
+  document.getElementById('loading').style.display = 'none';
+
   const errorDiv = document.createElement('div');
 
   //Get elements
